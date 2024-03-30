fc8e54254937bcd0d6b68050522972551774333a
Hadeel Safadi <nbhadeelsafadi@gmail.com>
2024-03-30 16:07:16 +0300

    Initial commit

--- script1.py
+++ script1.py
@@ -3,7 +3,6 @@ import requests
 
 # store the response from an API in response
 response = requests.get("https://api.github.com")
-
 # Checking if the response status
 if response.status_code == 200:
 	print("Successfully connected to GitHub API!")
